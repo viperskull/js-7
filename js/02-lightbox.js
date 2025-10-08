27da740081e12287beaa04e461741ad93a481c52
stefania m <shin3qi@gmail.com>
2025-10-08 18:49:00 +0300

    js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const liImage = galleryItems
+  .map(
+    (galleryItem) =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${galleryItem.original}">
+      <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}"  />
+   </a>
+</li>`
+  )
+  .join("");
+
+galleryContainer.insertAdjacentHTML("afterbegin", liImage);
+
+const lightbox = new SimpleLightbox(".gallery__item a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
